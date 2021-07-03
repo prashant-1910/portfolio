ac98c2865f0302d5d29cacf230fed9c3d4fe3c87
prashant689 <prashant.bangilwar@xelits.com>
2021-07-03 11:04:22 +0530

    final code

--- js/app.js
+++ js/app.js
@@ -6,4 +6,15 @@ $(document).ready(function(){
       appendDots:'.slider-dots',
       dotsClass:'dots'
     });
+
+  let hamberge = document.querySelector('.hamberg');
+  let times = document.querySelector('.times');  
+  let mobileNav = document.querySelector('.mobile-nav');
+  
+  hamberge.addEventListener('click',()=>{
+    mobileNav.classList.add('open');
+  });
+  times.addEventListener('click',()=>{
+    mobileNav.classList.remove('open');
+  });
   });
